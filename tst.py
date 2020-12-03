131090ca190042643137dffda9eb63f83a26b119
verenaheusser <veheusser@gmail.com>
2020-12-03 15:00:34 +0100

    tst

--- tst.py
+++ tst.py
@@ -1,10 +1,18 @@
-import torch
-cuda = torch.device('cuda') 
+# import torch
+# cuda = torch.device('cuda') 
 
-use_cuda = True
-if use_cuda:
-    dtype = torch.cuda.FloatTensor
+# use_cuda = True
+# if use_cuda:
+#     dtype = torch.cuda.FloatTensor
+# else:
+#     dtype = torch.FloatTensor
+
+# x = torch.rand(2,2).type(dtype)
+
+
+
+import tensorflow as tf 
+if tf.test.gpu_device_name(): 
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 else:
-    dtype = torch.FloatTensor
-
-x = torch.rand(2,2).type(dtype)
+    print("Please install GPU version of TF")
