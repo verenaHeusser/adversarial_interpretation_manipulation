cc5fa54926a73c7d67c5aa1bd72ff2b3b333c769
Verena Heusser <veheusser@gmail.com>
2021-01-29 14:38:41 +0100

    presentation template

---
+++ presentation/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/sh
+
+DATE_COVER=$(date "+%d %B %Y")
+
+SOURCE_FORMAT="markdown_strict\
++pipe_tables\
++backtick_code_blocks\
++auto_identifiers\
++strikeout\
++yaml_metadata_block\
++implicit_figures\
++all_symbols_escapable\
++link_attributes\
++smart\
++fenced_divs"
+
+# pandoc -s --dpi=300 --slide-level 2 --toc --listings --shift-heading-level=0 --columns=50 --template default.latex --pdf-engine xelatex -f "$SOURCE_FORMAT" -M date="$DATE_COVER" -V classoption:aspectratio=169 -V lang=en-US -t beamer presentation.md -o presentation.pdf
+
+pandoc -t beamer presentation.md -V aspectratio=169 -V theme:metropolis -o presentation.pdf
---
+++ presentation/presentation.md
@@ -0,0 +1,23 @@
+# Why you should not trust me
+
+# Motivation
+
+## First Slide
+
+Something
+
+## Second Slide
+
+- First
+
+* Second
+
+# Interpretation Methods
+
+## Terminology
+
+# Adversarial Setting
+
+## Adversarial Machine Learning
+
+# Fooling Methods
